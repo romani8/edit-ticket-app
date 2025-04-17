e452e4b7f4217666dc77303803766b93a3506142
Roman Isaenko <gm2roman@gmail.com>
2025-04-17 00:45:41 -0600

    Initial header added

---
+++ src/View/Partials/header.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Edit Ticket</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Edit Ticket</a>
+    </div>
+</nav>
